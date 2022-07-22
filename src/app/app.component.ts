430164632de4100f012ef7c95c06dd7e049c2b68
Kumar <mahesh.o.kumar@capgemini.com>
2022-07-22 13:22:05 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-list-ang';
+
+//  constructor(){
+//    setTimeout(() => {
+//     this.title="changed title"
+//   }, 2000);
+//  }
+
+}
